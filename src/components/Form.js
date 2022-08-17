98f612a32d504780bc4adaf70864945fb4bdda47
shreeparab1890 <shrikrishnaparab18@gmail.com>
2022-08-18 01:40:07 +0530

    added more utilities

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,20 +7,23 @@ const initialState = {
 
 export default function Form() {
   const [text, setText] = useState(initialState.text);
+  const [stop, setStop] = useState("");
 
   const handleClear = () => {
-    let newText = "";
-    setText(newText);
+    setText("");
+    setStop("");
   };
 
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    setStop("");
   };
 
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    setStop("");
   };
 
   const handleCamelCase = () => {
@@ -28,6 +31,7 @@ export default function Form() {
       .toLowerCase()
       .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
     setText(newText);
+    setStop("");
   };
 
   const handleCapCase = () => {
@@ -38,12 +42,235 @@ export default function Form() {
 
     const newText = arr.join(" ");
     setText(newText);
+    setStop("");
+  };
+
+  const handleAltCase = () => {
+    var midText = text.toLowerCase().split("");
+    console.log(midText);
+    for (var i = 0; i < midText.length; i += 2) {
+      midText[i] = midText[i].toUpperCase();
+      console.log(midText[i]);
+    }
+    const newText = midText.join("");
+    setText(newText);
+    setStop("");
+  };
+
+  const handleSenCase = () => {
+    let newText = text.charAt(0).toUpperCase() + text.slice(1);
+    setText(newText);
+    setStop("");
+  };
+
+  const handleTitleCase = () => {
+    let newText = text.replace(/\w\S*/g, function (t) {
+      return t.charAt(0).toUpperCase() + t.substr(1).toLowerCase();
+    });
+    setText(newText);
+    setStop("");
   };
 
   const handleGenrateText = () => {
     let count = 2;
     let newText = loremIpsum({ p: count });
     setText(newText.toString());
+    setStop("");
+  };
+
+  const handleSplit = () => {
+    let newText = text.split(" ");
+    setText(newText.join("\r\n"));
+    setStop("");
+  };
+
+  const handleJoin = () => {
+    let newText = "";
+    if (text.includes("\r\n")) {
+      newText = text.split("\r\n").join(" ");
+    } else {
+      newText = text.split("\n").join(" ");
+    }
+    setText(newText);
+    setStop("");
+  };
+
+  const handleReverse = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+    setStop("");
+  };
+
+  const handleRemoveEmpty = () => {
+    let regex = /^\s*$(?:\r\n?|\n)/gm;
+    let newText = text.replace(regex, "");
+    setText(newText);
+    setStop("");
+  };
+
+  const handleDuplicateLines = () => {
+    const newLineExpression = /\r\n|\n\r|\n|\r/g;
+    let newText = text
+      .split(newLineExpression)
+      .filter((item, index, array) => array.indexOf(item) === index)
+      .join("\n");
+    setText(newText);
+    setStop("");
+  };
+
+  const handleDuplicateWords = () => {
+    let newText = text
+      .split(" ")
+      .filter(function (item, i, allItems) {
+        return i == allItems.indexOf(item);
+      })
+      .join(" ");
+    setText(newText);
+    setStop("");
+  };
+
+  const handleStopWord = () => {
+    const stopwords = [
+      "i",
+      "me",
+      "my",
+      "myself",
+      "we",
+      "our",
+      "ours",
+      "ourselves",
+      "you",
+      "your",
+      "yours",
+      "yourself",
+      "yourselves",
+      "he",
+      "him",
+      "his",
+      "himself",
+      "she",
+      "her",
+      "hers",
+      "herself",
+      "it",
+      "its",
+      "itself",
+      "they",
+      "them",
+      "their",
+      "theirs",
+      "themselves",
+      "what",
+      "which",
+      "who",
+      "whom",
+      "this",
+      "that",
+      "these",
+      "those",
+      "am",
+      "is",
+      "are",
+      "was",
+      "were",
+      "be",
+      "been",
+      "being",
+      "have",
+      "has",
+      "had",
+      "having",
+      "do",
+      "does",
+      "did",
+      "doing",
+      "a",
+      "an",
+      "the",
+      "and",
+      "but",
+      "if",
+      "or",
+      "because",
+      "as",
+      "until",
+      "while",
+      "of",
+      "at",
+      "by",
+      "for",
+      "with",
+      "about",
+      "against",
+      "between",
+      "into",
+      "through",
+      "during",
+      "before",
+      "after",
+      "above",
+      "below",
+      "to",
+      "from",
+      "up",
+      "down",
+      "in",
+      "out",
+      "on",
+      "off",
+      "over",
+      "under",
+      "again",
+      "further",
+      "then",
+      "once",
+      "here",
+      "there",
+      "when",
+      "where",
+      "why",
+      "how",
+      "all",
+      "any",
+      "both",
+      "each",
+      "few",
+      "more",
+      "most",
+      "other",
+      "some",
+      "such",
+      "no",
+      "nor",
+      "not",
+      "only",
+      "own",
+      "same",
+      "so",
+      "than",
+      "too",
+      "very",
+      "s",
+      "t",
+      "can",
+      "will",
+      "just",
+      "don",
+      "should",
+      "now",
+    ];
+
+    let res = [];
+    let words = text.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      let word_clean = words[i].split(".").join("");
+      if (!stopwords.includes(word_clean)) {
+        res.push(word_clean);
+      }
+    }
+    let newText = res.join(" ");
+    setText(newText);
+    setStop(stopwords.join(" "));
   };
 
   const handleOnChange = (e) => {
@@ -52,7 +279,31 @@ export default function Form() {
   return (
     <>
       <div>
-        <h1>Enter The Text to Analyze</h1>
+        <div className="row">
+          <div className="col-6">
+            <h1>Enter The Text to Analyze</h1>
+          </div>
+
+          <div className="col-6" style={{ textAlign: "right" }}>
+            <button
+              type="button"
+              className="btn btn-light my-2 mx-2"
+              onClick={() => {
+                navigator.clipboard.writeText(text);
+              }}
+            >
+              Copy to Clipboard
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger my-2 mx-2"
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+          </div>
+        </div>
+
         <div className="mb-3">
           <textarea
             className="form-control"
@@ -63,47 +314,117 @@ export default function Form() {
             onChange={handleOnChange}
           ></textarea>
         </div>
-        <button
-          type="button"
-          className="btn btn-danger mx-2"
-          onClick={handleClear}
-        >
-          Clear
-        </button>
+        <div className="container">
+          <h3>
+            {stop ? "Stopwords: " + stop.split(" ").length + " words" : ""}
+          </h3>
+          <p>{stop}</p>
+        </div>
+
         <button
           type="button"
           className="btn btn-primary"
           onClick={handleUpClick}
         >
-          Convert to Uppercase
+          to UPPERCASE
         </button>
         <button
           type="button"
-          className="btn btn-primary mx-2"
+          className="btn btn-primary my-2 mx-2"
           onClick={handleLoClick}
         >
-          Convert to LowerCase
+          to lowercase
         </button>
         <button
           type="button"
-          className="btn btn-primary mx-2"
+          className="btn btn-primary my-2 mx-2"
           onClick={handleCamelCase}
         >
-          Convert to Camel Case
+          to camelCase
         </button>
         <button
           type="button"
-          className="btn btn-primary mx-2"
+          className="btn btn-primary my-2 mx-2"
           onClick={handleCapCase}
         >
-          Capitalize Case
+          to Capitalize Case
         </button>
         <button
           type="button"
-          className="btn btn-primary mx-2"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleAltCase}
+        >
+          to aLtErNaTe CaSe
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleSenCase}
+        >
+          to Sentence case
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleTitleCase}
+        >
+          to Title Case
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleSplit}
+        >
+          Split Text
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleJoin}
+        >
+          Join Text
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleReverse}
+        >
+          Reverse Text
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleRemoveEmpty}
+        >
+          Remove Empty LInes
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleDuplicateLines}
+        >
+          Remove Duplicate Lines
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleDuplicateWords}
+        >
+          Remove Duplicate Words
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleStopWord}
+        >
+          Remove Stop Words
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
           onClick={handleGenrateText}
         >
-          Generate Lorem Text
+          Generate Text
         </button>
       </div>
       <div className="container my-3">
@@ -111,7 +432,8 @@ export default function Form() {
         <p>
           {text === "" ? "0" : text.trim().split(" ").length}{" "}
           {text.trim().split(" ").length === 1 ? "word" : "words"} and{" "}
-          {text.length} Characters
+          {text.length} Characters and{" "}
+          {text === "" ? "0" : text.split(/\r\n|\r|\n/).length} Lines
         </p>
         <p>
           {" "}
